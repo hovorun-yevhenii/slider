8721a3436272371e20df6956d14430fd1a6c342c
oxygene4 <5797565@gmail.com>
2018-06-04 12:14:21 +0300

    panning preview

--- script/jquery.adaptiveCarousel.js
+++ script/jquery.adaptiveCarousel.js
@@ -14,21 +14,22 @@ Object.assign(Slider.prototype, {
     caption: false,
     autoPlay: false,
     autoPlayInterval: 500,
-    swipeThreshold: 35,
     direction: "horizontal",
     onSlideChanged: null,
+    animatedSlides: false,
 
     //Properties
     index: 0,
     isMoving: false,
-    autoPlayerCounter: null, 
+    autoPlayerCounter: null,
     
 	init() {
         this.sliderElement = $(this.sliderSelector)[0];    
         this.viewport = $(this.sliderElement).find(".slider__viewport")[0];
         this.slides = $(this.viewport).children();
-        this.swipeZone = new Hammer(this.viewport);
+        this.viewportSwipe = new Hammer(this.viewport);
         this.$find = selector => $(`${this.sliderSelector} ${selector}`);
+        this.viewportTransition = `transform ${this.animationDuration / 1000}s`;
    
         this.setupSize();
         
@@ -55,6 +56,29 @@ Object.assign(Slider.prototype, {
         this.updateControls(this.index);
     },
 
+    addListeners() {
+        $(window).resize(this.setupSize.bind(this))
+                 .keydown(this.handleKeyDown.bind(this));
+
+        this.viewportSwipe.on("pan", this.onViewportPan.bind(this))
+            .get("pan").set({ direction: Hammer[`DIRECTION_${this.direction.toUpperCase()}`] });
+
+        if (this.arrows) {
+            this.$find(`.arrow-prev`).click(this.movePrev.bind(this));
+            this.$find(`.arrow-next`).click(this.moveNext.bind(this)); 
+        }
+
+        if (this.pagination) {
+            this.$find(`.pagination-btn`).click(this.handlePaginationClick.bind(this));
+        }
+
+        if (this.preview) {
+            this.$find(`.preview-img`).click(this.handlePaginationClick.bind(this));
+            this.previewSwipe.on("pan", this.onPreviewPan.bind(this))
+                .get("pan").set({ direction: Hammer[`DIRECTION_${this.direction.toUpperCase()}`] });
+        }
+    },
+
     updateControls(index) {
         if (!this.loop) {
             const hasPrev = index === 0,
@@ -68,7 +92,8 @@ Object.assign(Slider.prototype, {
             const step = this.$find(`.preview-img`)[0].width,
                   previewPos = step * (index - 1.5);
             
-            this.$find(`.preview-panel`).scrollTo(previewPos, this.animationDuration);
+            this.previewPanel.scrollTo(previewPos, this.animationDuration);
+
             this.$find(`.preview-img`).removeClass("preview-img_active");
             this.$find(`[data-index=${index}]`).addClass("preview-img_active");
         }
@@ -86,6 +111,15 @@ Object.assign(Slider.prototype, {
         );
     },
 
+    setupSize() {
+        this.baseSize = this.sliderElement.offsetWidth;
+
+        $(this.viewport)
+            .css("width", `${this.baseSize * this.slides.length}px`)
+            .css("transition", this.viewportTransition)
+            .css("transform", `translateX(-${this.index * this.baseSize}px)`);
+    },
+
     renderCaption() {
         _.each(this.slides, (slide, counter) => {
             $('<p/>', { 
@@ -93,15 +127,6 @@ Object.assign(Slider.prototype, {
                 style: `--content: "${counter + 1}. ${$(slide).children("img")[0].alt}"`
             }).appendTo(slide);
         });
-    },
-
-    setupSize() {
-        this.baseSize = this.sliderElement.offsetWidth;
-
-        $(this.viewport)
-            .css("width", `${this.baseSize * this.slides.length}px`)
-            .css("transition", `transform ${this.animationDuration / 1000}s`)
-            .css("transform", `translateX(-${this.index * this.baseSize}px)`);
     },
 
     renderArrows() {
@@ -122,46 +147,31 @@ Object.assign(Slider.prototype, {
         });
     },
 
-    addListeners() {
-        $(window).resize(this.setupSize.bind(this))
-                 .keydown(this.handleKeyDown.bind(this));
-
-        this.swipeZone.on("pan", this.handlePan.bind(this))
-            .get("pan").set({ direction: Hammer[`DIRECTION_${this.direction.toUpperCase()}`] });
-
-        if (this.arrows) {
-            this.$find(`.arrow-prev`).click(this.movePrev.bind(this));
-            this.$find(`.arrow-next`).click(this.moveNext.bind(this)); 
-        }
-
-        if (this.pagination) {
-            this.$find(`.pagination-btn`).click(this.handlePaginationClick.bind(this));
-        }
-
-        if (this.preview) {
-            this.$find(`.preview-img`).click(this.handlePaginationClick.bind(this));
-        }
-    },
-
-    handlePan(pan) {
+    onViewportPan(pan) {
         const axis = this.direction === "horizontal" ? "deltaX" : "deltaY",
               delta = pan[axis] * -1,
               whereWeStart = this.index * this.baseSize,
               timeToChange = Math.abs(delta) > this.baseSize / 5;
 
         if (timeToChange) {
-            const direction = delta > 0 ? "Next" : "Prev";
-            this[`move${direction}`]();
-        //  delta > 0 ? this.moveNext() : this.movePrev();
+            delta > 0 ? this.moveNext() : this.movePrev();
         } else if (pan.isFinal) {
             $(this.viewport)
-                .css("transition", `transform ${this.animationDuration / 1000}s`)
+                .css("transition", this.viewportTransition)
                 .css("transform", `translateX(-${whereWeStart}px)`);
         } else {
             $(this.viewport)
                 .css("transition", "none")
                 .css("transform", `translateX(-${whereWeStart + delta}px)`);
         }
+    },
+
+    onPreviewPan(pan) {
+        const axis = this.direction === "horizontal" ? "deltaX" : "deltaY",
+              sign = Math.sign(pan[axis]),
+              scrollPos = this.previewPanel[0].scrollLeft;
+
+        this.previewPanel.scrollTo(scrollPos - 10 * sign);
     },
 
     handleKeyDown(key) {
@@ -192,25 +202,40 @@ Object.assign(Slider.prototype, {
         if (this.isMoving) return;
 
         const index = +$(click.target).attr("data-index");
+
         this.moveToSlide(index);
     },
 
     renderPreview() {
-        const previewPanel = $('<div/>', { 
+        this.previewPanel = $('<div/>', { 
             class: "preview-panel" 
         }).appendTo(this.sliderElement);
 
-        const previewBox = $('<div/>', {
+        const previewUnit = $('<div/>', {
             class: "preview-viewbox" 
-        }).appendTo(previewPanel);
+        }).appendTo(this.previewPanel);
 
         _.each(this.slides, (slide, counter) => {
             $('<img/>', { 
                 class: "preview-img",
                 src: $(slide).children("img")[0].src,
                 "data-index": counter
-            }).appendTo(previewBox);
+            }).appendTo(previewUnit);
         });
+
+        this.previewSwipe = new Hammer(this.previewPanel[0]);
+    },
+
+    startAnimation() {
+        this.$find(`.slider__slide`)
+            .css("transition", ".5s")
+            .css("transform", "skewY(180deg)");
+    },
+
+    endAnimation() {
+        this.$find(`.slider__slide`)
+            .css("transition", "none")
+            .css("transform", "skewY(0)");
     },
 
     moveToSlide(index) {
@@ -218,15 +243,20 @@ Object.assign(Slider.prototype, {
 
         this.index = index;
         this.isMoving = true;
+
+        if (this.animatedSlides) this.startAnimation();
         
         $(this.viewport)
-            .css("transition", `transform ${this.animationDuration / 1000}s`)
+            .css("transition", this.viewportTransition)
             .css("transform", `translateX(-${index * this.baseSize}px)`);
 
         this.updateControls(index);
         
         setTimeout(() => {
             if (this.onSlideChanged) this.onSlideChanged();
+
+            if (this.animatedSlides) this.endAnimation();
+            
             this.isMoving = false;
         },  this.animationDuration);
     }
@@ -238,24 +268,13 @@ const slider = new Slider({
     loop: false,
     arrows: true,
     pagination: true,
-    preview: 1,
+    preview: true,
     caption: true,
     autoPlay: false,
     autoPlayInterval: 500,
+    animatedSlides: true,
     direction: "horizontal"
 });
-
-// const slider1 = new Slider({
-//     sliderSelector: "#slider1",
-//     loop: false,
-//     arrows: true,
-//     pagination: true,
-//     preview: false,
-//     caption: true,
-//     autoPlay: false,
-//     autoPlayInterval: 500,
-//     onSlideChanged: userFunc
-// });
 
 function userFunc() {
     console.log([
